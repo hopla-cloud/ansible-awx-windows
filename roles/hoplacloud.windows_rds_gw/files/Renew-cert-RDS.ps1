6433c8de827e46d2e42dd24772dbe8704793401f
Joffrey <jskandera@iilyo.com>
2020-05-13 16:57:15 +0200

    Split RDS roles

---
+++ roles/hoplacloud.windows_rds_gw/files/Renew-cert-RDS.ps1
@@ -0,0 +1,13 @@
+﻿param($result)
+set-alias ps64 "$env:C:\Windows\System32\WindowsPowerShell\v1.0\Powershell.exe"
+
+ps64 -args $result -command {
+   $result = $args[0]
+   $pfxpath = $result.ManagedItem.CertificatePath
+
+   Import-Module RemoteDesktop
+   Set-RDCertificate -Role RDPublishing -ImportPath $pfxpath -Force
+   Set-RDCertificate -Role RDWebAcces -ImportPath $pfxpath -Force
+   Set-RDCertificate -Role RDGateway -ImportPath $pfxpath -Force
+   Set-RDCertificate -Role RDRedirector -ImportPath $pfxpath -Force
+}
