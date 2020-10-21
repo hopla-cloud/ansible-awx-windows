0502d9c7b9ca77261128ec9ecbe27b0e16c07559
Joffrey <jskandera@iilyo.com>
2020-10-21 10:07:45 +0200

    Change Renew let's encrypt script

--- roles/hoplacloud.windows_rds_gw/files/Renew-cert-RDS.ps1
+++ roles/hoplacloud.windows_rds_gw/files/Renew-cert-RDS.ps1
@@ -1,13 +1,20 @@
-﻿param($result)
-set-alias ps64 "$env:C:\Windows\System32\WindowsPowerShell\v1.0\Powershell.exe"
+param($result)
 
-ps64 -args $result -command {
-   $result = $args[0]
-   $pfxpath = $result.ManagedItem.CertificatePath
+$pfxpath = $result.ManagedItem.CertificatePath
+Import-Module RemoteDesktop
+$broker = (Get-RDServer -Role RDS-CONNECTION-BROKER).Server
 
-   Import-Module RemoteDesktop
-   Set-RDCertificate -Role RDPublishing -ImportPath $pfxpath -Force
-   Set-RDCertificate -Role RDWebAcces -ImportPath $pfxpath -Force
-   Set-RDCertificate -Role RDGateway -ImportPath $pfxpath -Force
-   Set-RDCertificate -Role RDRedirector -ImportPath $pfxpath -Force
-}
+Set-RDCertificate -Role RDPublishing -ImportPath $pfxpath -Force -ConnectionBroker $broker
+Set-RDCertificate -Role RDWebAcces -ImportPath $pfxpath -Force -ConnectionBroker $broker
+Set-RDCertificate -Role RDGateway -ImportPath $pfxpath -Force -ConnectionBroker $broker
+Set-RDCertificate -Role RDRedirector -ImportPath $pfxpath -Force -ConnectionBroker $broker
+Import-RDWebClientBrokerCert -Path $pfxpath
+
+Restart-Service TSGateway -Force -ErrorAction Stop
+Restart-Service TScPubRPC -Force -ErrorAction Stop
+Restart-Service Tssdis -Force -ErrorAction Stop
+Restart-Service w3svc -Force -ErrorAction Stop
+Restart-Service RasMan -Force -ErrorAction Stop
+Restart-Service RasAuto -Force -ErrorAction Stop
+Restart-Service RDMS -Force -ErrorAction Stop
+Restart-Service Termservice -Force -ErrorAction Stop
